dc9fa3c1ff9227e9bee81fa1b92afa56db7f9b5f
ashlyn-mcc <am4915a@american.edu>
2023-11-18 02:11:34 -0500

    Slider working

--- assignments/05_units/Assets/Scripts/GameManager.cs
+++ assignments/05_units/Assets/Scripts/GameManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager SharedInstance;
 
+    public Slider mySlider;
+
     fishScript selectedUnit;
 
-    public GameObject clownfishPrefab;
+    public GameObject pufferfishPrefab;
+    public GameObject bettafishPrefab;
+    public GameObject parrotfishPrefab;
+    public GameObject swordfishPrefab;
+    public GameObject lionfishPrefab;
 
     void Awake()
     {
@@ -35,11 +42,26 @@ public class GameManager : MonoBehaviour
             {
                 if (hit.collider.gameObject.layer == LayerMask.NameToLayer("glass"))
                 {
-                    
-                    GameObject fish = Instantiate(clownfishPrefab, hit.point, Quaternion.identity);
+                    GameObject fish = Instantiate(ChooseFish(), hit.point, Quaternion.identity);
             
                 }
             }
         }
     }
+
+    GameObject ChooseFish(){
+        if (mySlider.value == 0){
+            return pufferfishPrefab;
+        } else if (mySlider.value == 1){
+            return bettafishPrefab;
+        } else if (mySlider.value == 2){
+            return parrotfishPrefab;
+        } else if (mySlider.value == 3){
+            return swordfishPrefab;
+        } else {
+            return lionfishPrefab;
+        }
+    }
+
+    
 }
